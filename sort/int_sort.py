30ac9fae04c3fb204b1e0feb72b2a49d42bd62d4
Matthew Virgin <42557195+mvirgin@users.noreply.github.com>
2022-11-18 00:56:50 -0500

    added quick and insertion sort

--- sort/int_sort.py
+++ sort/int_sort.py
@@ -23,6 +23,9 @@ This module sorts lists of integers...
 
 def bubble(int_list):
     """
+    This function takes a list and sorts it by comparing each element to the
+    following element, swapping them when nescessary
+
     Args:
         int_list: This is the list of integers to be sorted
 
@@ -47,28 +50,80 @@ def bubble(int_list):
 
     print("bubble sort")
 
-def quick(int_list):
+def partition(int_list, left_index, right_index):
+    """
+    This function partitions the list around its last element.
+    As in, the list is modified so that every element less than the
+    last element is on the left of the last element, and every element
+    greater than the last element is on the right. The position of
+    the last element (the pivot point) after the list
+    is modified is returned
+
+    Args:
+        int_list: This is the list of integers to be sorted
+        left_index: The leftmost index of the given list
+        right_index: The rightmost index of the given list
+
+    Returns:
+        partition_index: The position of the pivot point after modification
+    """
+
+    pivot_point = int_list[right_index]
+    compare_ptr = left_index - 1
+
+    for i in range(left_index, right_index):
+        if int_list[i] <= pivot_point:
+            compare_ptr+=1
+            temp = int_list[compare_ptr]
+            int_list[compare_ptr] = int_list[i]
+            int_list[i] = temp
+
+    temp = int_list[compare_ptr+1]
+    int_list[compare_ptr+1] = int_list[right_index]
+    int_list[right_index] = temp
+
+    partition_index = compare_ptr + 1
+
+    return partition_index
+        
+def quick(int_list, left_index, right_index):
     """
     Args:
         int_list: This is the list of integers to be sorted
+        left_index: The leftmost index of the given list
+        right_index: The rightmost index of the given list
 
     Returns:
         nil
     """
-    print("quick sort")
+    if left_index < right_index:
+        partition_index = partition(int_list, left_index, right_index)
 
+        quick(int_list, left_index, partition_index - 1) # left of pivot
+        quick(int_list, partition_index + 1, right_index) # right of pivot
+
+    print("quick sort")
 
 def insertion(int_list):
     """
+    This function loops over the list, comparing each element to the next one,
+    inserting the next element in its proper position if it is out of order
+
     Args:
         int_list: This is the list of integers to be sorted
 
     Returns:
         nil
     """
-    print("insertion sort")
 
-    #git pull
-    # add files
-    # commit
-    # change whatever I need to
+    for i in range(1, len(int_list)):
+        current = int_list[i]
+        j = i - 1
+
+        while current < int_list[j] and j >= 0:
+            int_list[j + 1] = int_list[j]
+            j-=1
+
+        int_list[j + 1] = current
+
+    print("insertion sort")
